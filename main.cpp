ca02a8f0f3f5b445c40e96062acf40df6e9a7d85
RainzDev <michael08pokrovsski16@gmail.com>
2026-09-08 04:41:02 +0400

    fix error

--- main.cpp
+++ main.cpp
@@ -804,7 +804,7 @@ void rpcWorker(std::shared_ptr<discordpp::Client> client) {
                     activity.SetTimestamps(timestamps);
 
                     // Schedule the presence update asynchronously to avoid blocking the worker
-                    std::thread([client, activity, storedSongData]() mutable {
+                    std::thread([client, activity]() mutable {
                         std::this_thread::sleep_for(std::chrono::seconds(5));
                         updatePresence(client, activity, "quest", "Meta Quest", storedSongData.metadata["coverURL"]);
                     }).detach();
